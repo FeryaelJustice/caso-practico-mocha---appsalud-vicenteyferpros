79318f155cd0f44ac952af0e926be87adbcd63cd
Fernando Gonzalez Serrano <nano9gs@hotmail.es>
2021-11-11 20:36:13 +0100

    fix

--- src/bascula.js
+++ src/bascula.js
@@ -1,5 +1,24 @@
-const Bascula = {
-    
+class Bascula {
+
+    constructor(pesos, alturas, fechas, anotaciones) {
+        this.pesos = pesos;
+        this.alturas = alturas;
+        this.fechas = fechas;
+        this.anotaciones = anotaciones;
+    }
+
+    static describirIMC(peso, altura) {
+        let imc = peso / (altura * altura);
+        if (imc < 18.5) {
+            return "Bajo peso";
+        } else if (imc < 25) {
+            return "Normal";
+        } else if (imc < 30) {
+            return "Sobrepeso";
+        } else {
+            return "Obesidad";
+        }
+    }
 
 }
 
