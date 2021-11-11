1b931a75e7ec135cfcfc5c4c0a69643efcec7557
Vicente Pedro García De La Morena <vgarciamorena@cifpfbmoll.eu>
2021-11-11 21:05:02 +0100

    calcularIMC and describirIMC Tests

---
+++ test/paciente_test.js
@@ -0,0 +1,6 @@
+const { assert } = require('chai');
+var chai = require('chai');
+var should = require('chai').should();
+const Paciente = require("../src/paciente");
+const paciente1 = new Paciente();
+
