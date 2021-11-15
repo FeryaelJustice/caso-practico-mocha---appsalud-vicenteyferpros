24a0afa297af3c28c84e97d798b687fb1423fc9d
Fernando Gonzalez Serrano <nano9gs@hotmail.es>
2021-11-15 17:58:43 +0100

    Paciente logica de negocio finished

--- src/paciente.js
+++ src/paciente.js
@@ -1,4 +1,63 @@
+const Bascula = require('./bascula');
 class Paciente {
+
+    constructor(nombre, apellidos, fechaNacimiento) {
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.fechaNacimiento = fechaNacimiento;
+        this.registroBascula = new Bascula();
+    }
+
+    saludar() {
+        console.log(`Hola, soy ${this.nombre} ${this.apellidos}`);
+    }
+
+    obtenerNombre() {
+        return this.nombre;
+    }
+
+    modificarNombre(nombre) {
+        this.nombre = nombre;
+    }
+
+    obtenerApellidos() {
+        return this.apellidos;
+    }
+
+    modificarApellidos(apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    obtenerFechaNacimiento() {
+        return this.fechaNacimiento;
+    }
+
+    modificarFechaNacimento(fecha) {
+        this.fechaNacimiento = fecha;
+    }
+
+    obtenerEdad() {
+        let fechaActual = new Date();
+        let edad = fechaActual.getFullYear() - this.fechaNacimiento.getFullYear();
+        if (fechaActual.getMonth() < this.fechaNacimiento.getMonth()) {
+            edad--;
+        } else if (fechaActual.getMonth() == this.fechaNacimiento.getMonth() && fechaActual.getDate() < this.fechaNacimiento.getDate()) {
+            edad--;
+        }
+        return edad;
+    }
+
+    modificarBascula(bascula){
+        this.registroBascula = bascula;
+    }
+
+    obtenerBascula() {
+        return this.registroBascula;
+    }
+
+    calcularIMC() {
+        return this.registroBascula.calcularIMC()
+    }
 
 }
 
