7fec1341766c6dcd33489b08e3e0ca1dc07f56b9
Vicente Pedro García De La Morena <vgarciamorena@cifpfbmoll.eu>
2021-11-17 18:05:04 +0100

    New class Termometro

---
+++ src/termometro.js
@@ -0,0 +1,39 @@
+class Termometro {
+    constructor(temperaturas, fechas, anotaciones){
+        this.temperaturas = temperaturas;
+        this.fechas = fechas;
+        this.anotaciones = anotaciones;
+    };
+
+    obtenerNumeroAnotaciones(){
+
+    };
+
+    anotarTemperatura(){
+
+    };
+
+    obtenerTemperaturaMax(){
+
+    };
+
+    obtenerTemperaturaMin(){
+
+    };
+
+    obtenerTemperaturaMedia(){
+
+    };
+
+    obtenerTablaTemperaturasHTML(){
+
+    };
+
+    static convertirCelsiusAFahrenheit(){
+
+    };
+
+    static convertirFahrenheitACelsius(){
+        
+    }
+}
