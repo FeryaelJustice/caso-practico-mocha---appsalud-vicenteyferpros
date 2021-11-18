071d40fb3339e56039685143b731db2025851c29
Vicente Pedro García De La Morena <vgarciamorena@cifpfbmoll.eu>
2021-11-18 19:57:43 +0100

    Cambios en termometro

--- src/termometro.js
+++ src/termometro.js
@@ -4,38 +4,65 @@ class Termometro {
         this.temperaturas = temperaturas;
         this.fechas = fechas;
         this.anotaciones = anotaciones;
-    };
+    }
 
     obtenerNumeroAnotaciones(){
         return this.anotaciones;
-    };
+    }
 
-    anotarTemperatura(temperaturas, fechas){
-
-    };
+    anotarTemperatura(temperatura, fecha){
+        if (temperatura == undefined && temperatura == null) {
+            return "temperatura is not defined";
+        }
+        this.temperaturas.push(temperatura);
+        if (fecha !== undefined && fecha != null) {
+            this.fechas.push(fecha);
+        }
+        this.anotaciones += 1;
+    }
 
     obtenerTemperaturaMax(){
-
-    };
+        let temperaturaMax = this.temperaturas[0];
+        for (let i = 0; i < this.temperaturas.length; i++) {
+            if (this.temperaturas[i] > temperaturaMax) {
+                temperaturaMax = this.temperaturas[i];
+            }
+        }
+        return temperaturaMax;
+    }
 
     obtenerTemperaturaMin(){
-
-    };
+        let temperaturaMin = this.temperaturas[0];
+        for (let i = 0; i < this.temperaturas.length; i++) {
+            if (this.temperaturas[i] < temperaturaMin) {
+                temperaturaMin = this.temperaturas[i];
+            }
+        }
+        return temperaturaMin;
+    }
 
     obtenerTemperaturaMedia(){
-
-    };
+        
+    }
 
     obtenerTablaTemperaturasHTML(){
 
-    };
+    }
 
     static convertirCelsiusAFahrenheit(){
-
-    };
+        let fahrenheit = ((temperaturas[0] * 9)/5)+32;
+        for (let i = 0; i < this.temperaturas.length; i++) {
+            
+        }
+        return fahrenheit;
+    }
 
     static convertirFahrenheitACelsius(){
+        let celsius = ((temperaturas[0] - 32)*5)/9;
+        for (let i = 0; i < this.temperaturas.length; i++) {
 
+        }
+        return celsius;
     }
 }
 
